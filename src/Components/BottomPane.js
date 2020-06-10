369f646282806c0dda6dc203ebb0cca7af0e6ebd
unknown <rnotaria776@gmail.com>
2020-06-09 19:57:48 -0500

    created custom button to follow pane during open/close

--- src/Components/BottomPane.js
+++ src/Components/BottomPane.js
@@ -1,19 +1,129 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SlidingPane from "../utils/react-sliding-pane/react-sliding-pane";
+import styled, { keyframes } from "styled-components";
 import "../utils/react-sliding-pane/react-sliding-pane.css";
 
-export default function BottomPane({ height, marginTop }) {
+const translateUp = (height) => keyframes`
+from {
+  transform: translateY(0);
+}
+to {
+  transform: translateY(-${height});
+}
+`;
+const TranslateUp = styled.div`
+  // display: inline-block;
+  animation: ${(props) => translateUp(props.height)} ${(props) => props.delay}s
+    ease forwards;
+`;
+
+const translateDown = (height) => keyframes`
+from {
+  transform: translateY(-${height});
+}
+to {
+  transform: translateY(0);
+}
+`;
+const TranslateDown = styled.div`
+  // display: inline-block;
+  animation: ${(props) => translateDown(props.height)}
+    ${(props) => props.delay}s;
+`;
+
+const closedButtonStyle = () => ({
+  position: "absolute",
+  left: "50%",
+  bottom: "0",
+  transform: "translate(-50%)",
+  zIndex: 1,
+});
+
+const openedButtonStyle = (height) => ({
+  position: "absolute",
+  left: "50%",
+  bottom: height,
+  transform: "translate(-50%)",
+  zIndex: 1,
+});
+
+export default function BottomPane({ height, marginTop, delay = 0.5 }) {
+  console.log("BottomPane");
+
   const [open, setOpen] = useState(false);
+  const [opened, setOpened] = useState(false);
+  const [closed, setClosed] = useState(true);
+
+  const renderButton = () => {
+    var buttonJSX;
+    if (open === false && opened === false && closed === true) {
+      buttonJSX = (
+        <div style={closedButtonStyle()}>
+          <button
+            onClick={() => {
+              setOpen(true);
+              setClosed(false);
+            }}
+          >
+            Click1
+          </button>
+        </div>
+      );
+    } else if (open === true && opened === false && closed === false) {
+      buttonJSX = (
+        <div style={closedButtonStyle()}>
+          <TranslateUp delay={delay} height={height}>
+            <button>Click2</button>
+          </TranslateUp>
+        </div>
+      );
+    } else if (open === true && opened === true && closed === false) {
+      buttonJSX = (
+        <div style={openedButtonStyle(height)}>
+          <button
+            onClick={() => {
+              setOpen(false);
+              setOpened(false);
+            }}
+          >
+            Click3
+          </button>
+        </div>
+      );
+    } else if (open === false && opened === false && closed === false) {
+      console.log("Click4");
+      buttonJSX = (
+        <div style={closedButtonStyle()}>
+          <TranslateDown delay={delay} height={height}>
+            <button>Click4</button>
+          </TranslateDown>
+        </div>
+      );
+    }
+
+    return buttonJSX;
+  };
+
+  useEffect(() => {
+    var id;
+
+    if (open === true && opened === false && closed === false) {
+      id = window.setTimeout(() => {
+        setOpened(true);
+      }, delay * 1000);
+    } else if (open === false && opened === false && closed === false) {
+      id = window.setTimeout(() => {
+        setClosed(true);
+      }, delay * 1000);
+    }
+    return () => {
+      clearTimeout(id);
+    };
+  }, [open, opened, closed, delay]);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <button onClick={() => setOpen(true)}>Click</button>
+    <div>
+      {renderButton()}
       <SlidingPane
         from="bottom"
         className="some-custom-class"
@@ -26,7 +136,7 @@ export default function BottomPane({ height, marginTop }) {
         height={height}
         marginTop={marginTop}
       >
-        <div>And I am pane content. BTW, what rocks?</div>
+        <div></div>
       </SlidingPane>
     </div>
   );
--- src/utils/react-sliding-pane/react-sliding-pane.js
+++ src/utils/react-sliding-pane/react-sliding-pane.js
@@ -14,7 +14,7 @@ function ReactSlidingPane(_ref) {
     children = _ref.children,
     className = _ref.className,
     overlayClassName = _ref.overlayClassName,
-    closeIcon = _ref.closeIcon,
+    // closeIcon = _ref.closeIcon,
     _ref$from = _ref.from,
     from = _ref$from === void 0 ? "right" : _ref$from,
     width = _ref.width,
@@ -98,24 +98,24 @@ ReactSlidingPane.propTypes = {
   overlayClassName: PropTypes.string,
   from: PropTypes.oneOf(["left", "right", "bottom"]),
   width: PropTypes.string,
-  closeIcon: PropTypes.any,
+  // closeIcon: PropTypes.any,
   shouldCloseOnEsc: PropTypes.bool,
 };
 
-function IconClose() {
-  return /*#__PURE__*/ React.createElement(
-    "svg",
-    {
-      xmlns: "http://www.w3.org/2000/svg",
-      viewBox: "0 0 13 22",
-    },
-    /*#__PURE__*/ React.createElement("path", {
-      fill: "currentColor",
-      fillRule: "evenodd",
-      d:
-        "M4 11l8 8c.6.5.6 1.5 0 2-.5.6-1.5.6-2 0l-9-9c-.6-.5-.6-1.5 0-2l9-9c.5-.6 1.5-.6 2 0 .6.5.6 1.5 0 2l-8 8z",
-    })
-  );
-}
+// function IconClose() {
+//   return /*#__PURE__*/ React.createElement(
+//     "svg",
+//     {
+//       xmlns: "http://www.w3.org/2000/svg",
+//       viewBox: "0 0 13 22",
+//     },
+//     /*#__PURE__*/ React.createElement("path", {
+//       fill: "currentColor",
+//       fillRule: "evenodd",
+//       d:
+//         "M4 11l8 8c.6.5.6 1.5 0 2-.5.6-1.5.6-2 0l-9-9c-.6-.5-.6-1.5 0-2l9-9c.5-.6 1.5-.6 2 0 .6.5.6 1.5 0 2l-8 8z",
+//     })
+//   );
+// }
 
 export default ReactSlidingPane;
